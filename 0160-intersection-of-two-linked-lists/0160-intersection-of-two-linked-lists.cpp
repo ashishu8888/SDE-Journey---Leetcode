7e4564e93be2882f04ecbb15f4f4e23f384bbdea
Ashish Upadhyay <81767933+ashishu8888@users.noreply.github.com>
2022-10-23 01:24:53 +0530

    Time: 213 ms (11.35%), Space: 14.7 MB (32.78%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
@@ -10,24 +10,68 @@
 class Solution {
 public:
     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
-        ListNode* curr = headA;
-        map<const ListNode *,int> m;
+        ListNode* curr = headA,*curr1=NULL;
+       int a=0,b=0;
         while(curr!=NULL)
         {
-            m[curr]++;
+            //m[curr]++;
             curr = curr->next;
+            a++;
         }
         
         curr = headB;
         
         while(curr!=NULL)
         {
-            if(m[curr])
-            {
-                return curr;
-            }
+            
             curr=curr->next;
+            b++;
         }
+        
+            curr =  headA;
+            curr1 = headB;
+        //cout<<a<<b<<endl;
+        if(a>b)
+        {
+             int c = a-b;
+           // curr = headA;
+            while(c--)
+            {
+                curr = curr->next;
+            }
+        }
+        
+        else if(a<b)
+        {
+            int c = b-a;
+           // curr = headA;
+            while(c--)
+            {
+                curr1 = curr1->next;
+            }
+        }
+        
+             cout<<curr->val<<" "<<curr1->val<<endl;  
+           // ListNode* curr1 = headB;
+            
+            while(curr!=NULL and curr1!=NULL)
+            {
+                //cout<<"f"<<endl;
+                if(curr == curr1)
+                {
+                    return curr;
+                }
+                
+                else{
+                     cout<<curr->val<<" "<<curr1->val<<endl;  
+                    curr = curr->next;
+                    curr1=curr1->next;
+                }
+                
+            }
+        
+        
+        
         
         return NULL;
         
