aea856c76bf0650136e73d27aca4a3327babb3cd
Ashish Upadhyay <81767933+ashishu8888@users.noreply.github.com>
2022-11-01 22:52:53 +0530

    Time: 619 ms (5.07%), Space: 29.2 MB (5.91%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.cpp
+++ 0496-next-greater-element-i/0496-next-greater-element-i.cpp
@@ -1,36 +1,51 @@
 class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+       
         vector<int> v;
-        for(int i=0;i<nums1.size();i++){
-            bool a = true;
-            int j = 0;
-            while(j<nums2.size()){
-                if(nums1[i] == nums2[j]){
-                    a = false;
-                    break;
+        for(int i=0;i<nums1.size();i++)
+        {
+             stack<int> s;
+            for(int j=nums2.size()-1;j>=0;j--)
+            {
+                if(s.empty())
+                {
+                    if(nums2[j] == nums1[i])
+                         {
+                                v.push_back(-1);
+                         }
+                    s.push(nums2[j]);
                 }
-                j++;
-            }
-            if(a == false){
-                bool b = true;
-                while(j<nums2.size()){
-                    if(nums2[j] > nums1[i]){
-                        v.push_back(nums2[j]);
-                        b = false;
-                        break;
+                else if(s.top() <= nums2[j])
+                {
+                    while(!s.empty() and s.top() <= nums2[j])
+                    {
+                        s.pop();
                     }
-                    j++;
+                    if(nums2[j] == nums1[i])
+                {
+                    if(s.empty())
+                    {
+                        v.push_back(-1);
+                    }
+                        else{
+                           v.push_back(s.top()); 
+                        }
                 }
-                if(b == true)
-                    v.push_back(-1);
-            }
-            
-            else{
-                v.push_back(-1);
+                    s.push(nums2[j]);
+                    
+                }
+                else if(s.top() > nums2[j])
+                {
+                         if(nums2[j] == nums1[i])
+                         {
+                                v.push_back(s.top());
+                         }
+                    s.push(nums2[j]);
+                }
+                
             }
         }
-        
         return v;
     }
 };
