f59d9ce9be454b7607b4089ad53acca85af2b14e
Ashish Upadhyay <81767933+ashishu8888@users.noreply.github.com>
2022-11-01 19:02:30 +0530

    Time: 49 ms (13.04%), Space: 8.2 MB (98.66%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.cpp
+++ 0496-next-greater-element-i/0496-next-greater-element-i.cpp
@@ -2,41 +2,35 @@ class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
         vector<int> v;
-        
-        for(int i=0;i<nums1.size();i++)
-        {
-            for(int j=0;j<nums2.size();j++)
-            {
-                if(nums1[i] == nums2[j])
-                {
-                    bool f = true;
-                    for(int k=j;k<nums2.size();k++)
-                    {
-                        if(nums2[k] > nums2[j])
-                        {
-                            f = false;
-                            v.push_back(nums2[k]);
-                            break;
-                        }
-                    }
-                    if(f)
-                    {
-                        v.push_back(-1);
-                    }
+        for(int i=0;i<nums1.size();i++){
+            bool a = true;
+            int j = 0;
+            while(j<nums2.size()){
+                if(nums1[i] == nums2[j]){
+                    a = false;
+                    break;
                 }
+                j++;
+            }
+            if(a == false){
+                bool b = true;
+                while(j<nums2.size()){
+                    if(nums2[j] > nums1[i]){
+                        v.push_back(nums2[j]);
+                        b = false;
+                        break;
+                    }
+                    j++;
+                }
+                if(b == true)
+                    v.push_back(-1);
+            }
+            
+            else{
+                v.push_back(-1);
             }
         }
         
-       return v;
-        
+        return v;
     }
 };
-
-
-
-
-
-
-
-
-
