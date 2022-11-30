33c6009e2993e618984df235a8daf5a016a343d3
Ashish Upadhyay <81767933+ashishu8888@users.noreply.github.com>
2022-11-30 18:18:08 +0530

    Time: 105 ms (5.05%), Space: 35.7 MB (5.13%) - LeetHub

---
+++ 0538-convert-bst-to-greater-tree/0538-convert-bst-to-greater-tree.cpp
@@ -0,0 +1,74 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> v;
+     map<int,int> m;
+    void in(TreeNode* root)
+    {
+        if(!root) return ;
+        
+        in(root->left);
+        
+        v.push_back(root->val);
+        
+        in(root->right);
+        
+    }
+    
+     void as(TreeNode* root)
+    {
+        if(!root) return ;
+        
+        as(root->left);
+        
+        root->val = m[root->val];
+        
+        as(root->right);
+        
+    }
+    
+    TreeNode* convertBST(TreeNode* root) {
+        if(!root)
+        {
+            return NULL;
+        }
+        
+        else if(root->left == NULL and root->right== NULL)
+        {
+            return root;
+        }
+        
+        in(root);
+        
+        int pre[v.size()];
+        pre[0] = v[0];
+        
+        for(int i=1;i<v.size();i++)
+        {
+            pre[i] = pre[i-1] + v[i];
+        }
+        
+       
+        int x = pre[v.size()-1];
+        
+        for(int i=0;i<v.size();i++)
+        {
+            m[v[i]] = x - pre[i] + v[i];
+        }
+        
+        as(root);
+        
+          return root;
+        
+    }
+};
