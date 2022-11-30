0cdf3ba18db35bea0b99c19b56c17ed22fa735ab
Ashish Upadhyay <81767933+ashishu8888@users.noreply.github.com>
2022-11-30 18:28:01 +0530

    Time: 79 ms (42.47%), Space: 33.5 MB (60.22%) - LeetHub

--- 0538-convert-bst-to-greater-tree/0538-convert-bst-to-greater-tree.cpp
+++ 0538-convert-bst-to-greater-tree/0538-convert-bst-to-greater-tree.cpp
@@ -11,64 +11,15 @@
  */
 class Solution {
 public:
-    vector<int> v;
-     map<int,int> m;
-    void in(TreeNode* root)
-    {
-        if(!root) return ;
-        
-        in(root->left);
-        
-        v.push_back(root->val);
-        
-        in(root->right);
-        
-    }
-    
-     void as(TreeNode* root)
-    {
-        if(!root) return ;
-        
-        as(root->left);
-        
-        root->val = m[root->val];
-        
-        as(root->right);
-        
-    }
-    
+    int sum = 0;
     TreeNode* convertBST(TreeNode* root) {
-        if(!root)
-        {
-            return NULL;
-        }
+        if(!root) return root;
+        convertBST(root->right);
+        sum += root->val;
+        root->val = sum;
+        convertBST(root->left);
         
-        else if(root->left == NULL and root->right== NULL)
-        {
-            return root;
-        }
-        
-        in(root);
-        
-        int pre[v.size()];
-        pre[0] = v[0];
-        
-        for(int i=1;i<v.size();i++)
-        {
-            pre[i] = pre[i-1] + v[i];
-        }
-        
-       
-        int x = pre[v.size()-1];
-        
-        for(int i=0;i<v.size();i++)
-        {
-            m[v[i]] = x - pre[i] + v[i];
-        }
-        
-        as(root);
-        
-          return root;
+        return root;
         
     }
 };
