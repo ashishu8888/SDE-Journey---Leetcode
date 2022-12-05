bf052dcf8b6da8f2ef256073f35ee0070bce3605
Ashish Upadhyay <81767933+ashishu8888@users.noreply.github.com>
2022-12-05 19:37:34 +0530

    Time: 168 ms (6.29%), Space: 64.2 MB (7.62%) - LeetHub

--- 0919-complete-binary-tree-inserter/0919-complete-binary-tree-inserter.cpp
+++ 0919-complete-binary-tree-inserter/0919-complete-binary-tree-inserter.cpp
@@ -18,60 +18,7 @@ public:
     
     }
     
-    int insert(int val) {
-     
-        queue<TreeNode*> q;
-        q.push(r);
- 
-        while(!q.empty())
-        {
-            int s = q.size();
-            bool o = false;
-            for(int i=0;i<s;i++)
-            {
-                TreeNode* a = q.front();
-                
-                q.pop();
-                
-                if(a->left)
-                    q.push(a->left);
-                
-                if(a->right)
-                    q.push(a->right);
-                    
-                    
-                    if(a->left == NULL)
-                    {
-                       
-                        TreeNode* w = new TreeNode(val);
-                        a->left = w;
-                      return a->val;
-                    }
-                    else if(a->right == NULL)
-                    {
-                       
-                        TreeNode* w = new TreeNode(val);
-                        a->right = w;
-                        return a->val;
-                    }
-                
-                
-                
-                
-            }
-        
-                
-            
-            
-        }
-    
-        return -1;
-        
-    }
-    
-    TreeNode* get_root() {
-     return r;   
-    }
+    int insert(int val) {  queue<TreeNode*> q; q.push(r);  while(!q.empty()) { int s = q.size(); bool o = false; for(int i=0;i<s;i++) { TreeNode* a = q.front();  q.pop();  if(a->left) q.push(a->left);  if(a->right) q.push(a->right);   if(a->left == NULL) {  TreeNode* w = new TreeNode(val); a->left = w; return a->val; } else if(a->right == NULL) {  TreeNode* w = new TreeNode(val); a->right = w; return a->val; }     }     }  return -1;  }  TreeNode* get_root() { return r;    }
 };
 
 /**
